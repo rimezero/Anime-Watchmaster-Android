f7aae52546e73a914b6f0d7155d7dafa21b5f28a
abraham <piperidis3@yahoo.gr>
2016-07-03 23:41:31 +0300

    upcoming start

--- app/src/main/java/com/example/admin/animewatchmaster/activities/animebyletter/AnimeLetterAdapter.java
+++ app/src/main/java/com/example/admin/animewatchmaster/activities/animebyletter/AnimeLetterAdapter.java
@@ -1,6 +1,9 @@
 package com.example.admin.animewatchmaster.activities.animebyletter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,7 @@ import com.example.admin.animewatchmaster.utils.databaseUtils.DBHelper;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 import me.grantland.widget.AutofitTextView;
 
@@ -33,7 +37,7 @@ public class AnimeLetterAdapter extends ArrayAdapter<Anime> {
     @Override
     public View getView(int position,View convertView,ViewGroup parent) {
 
-        Anime model = getItem(position);
+        final Anime model = getItem(position);
 
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_anime_by_letter_row,parent,false);
@@ -139,18 +143,178 @@ public class AnimeLetterAdapter extends ArrayAdapter<Anime> {
 
             Picasso.with(getContext())
                     .load(model.getImgurl())
-                    .resize(160,200)
+                    .fit()
                     .into(imageView);
 
 
             int animeId = model.getId();
-            DBHelper dbHelper = DBHelper.getInstance(getContext());
+            final DBHelper dbHelper = DBHelper.getInstance(getContext());
             LinearLayout linearLayoutbookmark = (LinearLayout) convertView.findViewById(R.id.bookmarklinear);
             if(dbHelper.checkIfExistsInWatchlist(animeId) || dbHelper.checkIfExistsInWatchedList(animeId) || dbHelper.checkIfExistsInWatchLaterList(animeId)) {
                 linearLayoutbookmark.setVisibility(View.VISIBLE);
             } else {
                 linearLayoutbookmark.setVisibility(View.GONE);
             }
+
+
+            final LinearLayout morelinear = (LinearLayout)convertView.findViewById(R.id.morelinear);
+
+            titlestart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(morelinear.getVisibility() == View.VISIBLE) {
+
+                        morelinear.setVisibility(View.GONE);
+
+                    }
+
+                }
+            });
+
+            ImageView moreimage = (ImageView)convertView.findViewById(R.id.moreimage);
+
+            moreimage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(morelinear.getVisibility() == View.VISIBLE) {
+
+                        morelinear.setVisibility(View.GONE);
+
+                    } else {
+
+                        morelinear.setAlpha(0f);
+                        morelinear.setVisibility(View.VISIBLE);
+                        morelinear.animate()
+                                .alpha(1f)
+                                .setDuration(400)
+                                .start();
+                    }
+
+                }
+            });
+
+
+            final ImageView watchlistimage = (ImageView)convertView.findViewById(R.id.watchlistaddimage);
+            final ImageView watchlaterimage = (ImageView)convertView.findViewById(R.id.watchlaterimage);
+            final ImageView watchedimage = (ImageView)convertView.findViewById(R.id.watchedimage);
+
+            final Bitmap bitmapDone = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_done_white_24dp);
+            Bitmap bitmapAdd = BitmapFactory.decodeResource(getContext().getResources(),R.drawable.ic_add_circle_outline_white_24dp);
+
+
+            if(dbHelper.checkIfExistsInWatchlist(model.getId())) {
+                watchlistimage.setImageBitmap(bitmapDone);
+            } else {
+                watchlistimage.setImageBitmap(bitmapAdd);
+            }
+
+
+            if(dbHelper.checkIfExistsInWatchLaterList(model.getId())) {
+                watchlaterimage.setImageBitmap(bitmapDone);
+            } else {
+                watchlaterimage.setImageBitmap(bitmapAdd);
+            }
+
+
+            if(dbHelper.checkIfExistsInWatchedList(model.getId())) {
+                watchedimage.setImageBitmap(bitmapDone);
+            } else {
+                watchedimage.setImageBitmap(bitmapAdd);
+            }
+
+
+            AutofitTextView addwatched = (AutofitTextView)convertView.findViewById(R.id.addwatched);
+            AutofitTextView addwatchlist = (AutofitTextView)convertView.findViewById(R.id.addwatchlist);
+            AutofitTextView addwatchlater = (AutofitTextView)convertView.findViewById(R.id.addwatchlater);
+            AutofitTextView addmore = (AutofitTextView)convertView.findViewById(R.id.addmore);
+
+
+            addmore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    morelinear.setVisibility(View.GONE);
+                    //future staff like share..
+                }
+            });
+
+            addwatched.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    morelinear.setVisibility(View.GONE);
+                    //do dbstuff
+                    if(!dbHelper.checkIfExistsInWatchedList(model.getId())) {
+                        dbHelper.insertIntoWatchedlist(model.getId());
+                    }
+
+                    watchedimage.setImageBitmap(bitmapDone);
+
+                }
+            });
+
+
+            addwatchlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    morelinear.setVisibility(View.GONE);
+                    //do dbstuff
+                    if(!dbHelper.checkIfExistsInWatchlist(model.getId())) {
+                        dbHelper.insertIntoWatchlist(model.getId(),0,0,"");
+
+                        Anime anime = dbHelper.getAnimeInfo(model.getId());
+
+                        String episodes = anime.getEpisodes();
+                        int ep = 0;
+                        boolean doUpdateFlag = false;
+
+                        if(episodes!=null && episodes.trim().equals("Ongoing")){
+                            doUpdateFlag = true;
+                        }else if(episodes.trim().isEmpty()){
+                            //do nothing
+                        }else if(episodes.contains("+")){
+                            StringTokenizer eps = new StringTokenizer(episodes.trim(),"+");
+                            try {
+                                double d = Double.valueOf(eps.nextToken());
+                                ep = (int) d;
+                            }catch (NumberFormatException e){
+                                Log.e("AnimeInfo - addToWlist", " Add to watchlist - Number format exception trying to parse string to double / episodes contains '+'");
+                            }
+                        }
+                        else{
+                            try {
+                                double d = Double.valueOf(episodes.trim());
+                                ep = (int) d;
+                            }catch (NumberFormatException e){
+                                Log.e("AnimeInfo - addToWlist"," Add to watchlist - Number format exception trying to parse string to double");
+                            }
+                        }
+
+                        dbHelper.insertIntoWatchlist(anime.getId(), 0, ep, "");
+
+                        watchlistimage.setImageBitmap(bitmapDone);
+                    }
+
+                }
+            });
+
+
+            addwatchlater.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    morelinear.setVisibility(View.GONE);
+                    //do dbstuff
+                    if(!dbHelper.checkIfExistsInWatchLaterList(model.getId())) {
+                        dbHelper.insertIntoWatchlaterlist(model.getId());
+                    }
+
+                    watchlaterimage.setImageBitmap(bitmapDone);
+
+                }
+            });
 
 
         } catch (Exception ex) {
