4cce4cf968469ade0e556ded2f33f819ff0a40ec
abraham <piperidis3@yahoo.gr>
2016-07-18 05:57:29 +0300

    facebook api implemented

--- app/src/main/java/com/evolsoft/animewatchmaster/activities/topanime/TopAnimeActivity.java
+++ app/src/main/java/com/evolsoft/animewatchmaster/activities/topanime/TopAnimeActivity.java
@@ -1,7 +1,11 @@
 package com.evolsoft.animewatchmaster.activities.topanime;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.evolsoft.animewatchmaster.R;
 import com.evolsoft.animewatchmaster.model.TopanimeModel;
@@ -10,6 +14,7 @@ import com.facebook.appevents.AppEventsLogger;
 import com.twotoasters.jazzylistview.JazzyGridView;
 import com.twotoasters.jazzylistview.effects.SlideInEffect;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,28 +22,88 @@ import java.util.List;
  */
 public class TopAnimeActivity extends AppCompatActivity {
 
+    private List<TopanimeModel> animeListState = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.layout_top_anime);
 
-        JazzyGridView jazzyGridView = (JazzyGridView)findViewById(R.id.gridview);
         List<TopanimeModel> topanimeModelList = DBHelper.getInstance(getApplicationContext()).getTopAnimeData();
 
         if(topanimeModelList != null && !topanimeModelList.isEmpty()) {
 
-            TopAnimeAdapter topAnimeAdapter = new TopAnimeAdapter(getApplicationContext(), topanimeModelList);
-            jazzyGridView.setTransitionEffect(new SlideInEffect());
-            jazzyGridView.setAdapter(topAnimeAdapter);
+            loadGridView(topanimeModelList,-1);
 
         } else {
             finish();
         }
 
 
+    }
+
+
+    private void loadGridView(List<TopanimeModel> animeList, int colsNum){
+        animeListState = animeList;
+
+        final JazzyGridView gridView = (JazzyGridView) findViewById(R.id.gridview);
+
+        if(colsNum == 1) {
+            gridView.setNumColumns(1);
+        } else if(colsNum == 2) {
+            gridView.setNumColumns(2);
+        } else if(colsNum == 3) {
+            gridView.setNumColumns(3);
+        }
+
+        TopAnimeAdapter topAnimeAdapter = new TopAnimeAdapter(getApplicationContext(), animeList);
+        gridView.setTransitionEffect(new SlideInEffect());
+        gridView.setAdapter(topAnimeAdapter);
+
+        gridView.setAdapter(topAnimeAdapter);
 
     }
+
+
+    public void listgridswitch(View v) {
+
+
+        JazzyGridView gridView = (JazzyGridView) findViewById(R.id.gridview);
+
+        if(!animeListState.isEmpty() && gridView.getVisibility() == View.VISIBLE) {
+
+            ImageView imageView = (ImageView) findViewById(R.id.imagebtnswitch);
+
+            Bitmap bitmap;
+
+            if(gridView.getNumColumns() == 3) {
+
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_apps_black_24dp);
+                imageView.setImageBitmap(bitmap);
+
+                loadGridView(animeListState, 2);
+
+            } else if (gridView.getNumColumns() == 2) {
+
+                bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_apps_black_24dp);
+                imageView.setImageBitmap(bitmap);
+
+                loadGridView(animeListState, 1);
+
+            } else {
+
+                bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_list_black_24dp);
+                imageView.setImageBitmap(bitmap);
+
+                loadGridView(animeListState,3);
+
+            }
+
+        }
+
+
+    }
+
 
 
     @Override
